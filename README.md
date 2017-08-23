9fd18bad8d8a50244465573f23d7515103058d02
Aurelia Friska <aurelia_friska@yahoo.com>
2017-08-23 12:57:17 +0700

    newtheme

--- README.md
+++
@@ -1,1 +0,0 @@
-# otion-cbn.github.io
--- js/main.js
+++
@@ -1,9 +0,0 @@
-
-function toggleChevron(e) {
-    $(e.target)
-        
-        .find("span.glyphicon")
-        .toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
-}
-$('.list-group-item').on('hidden.bs.collapse', toggleChevron);
-$('.list-group-item').on('shown.bs.collapse', toggleChevron);
